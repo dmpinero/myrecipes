2b22b860f0614552e154f5ad0b7051043c4044fe
Daniel Martínez Piñero <dmpinero@gmail.com>
2015-09-30 14:55:07 +0000

    Create new and edit forms and partials

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home' # Redirección de la raiz de la aplicación al controlador pages y método home
   get '/home', to:'pages#home' # Redirección al controlador pages y método home
   
+  
+  # Crea todas las rutas para las recetas
+  resources :recipes
 end
